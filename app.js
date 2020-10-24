528d7c5c21fe48fb5380548a1eb7c0a5ba9feb5a
ericerodgers <ericerodgers@yahoo.com>
2020-10-24 15:47:28 -0400

    complete project

--- app.js
+++ app.js
@@ -1,3 +1,48 @@
 const holes = document.querySelectorAll(".hole");
 const scoreBoard = document.querySelector(".score");
 const moles = document.querySelectorAll(".mole");
+const startButton = document.querySelector("#start-button");
+let lastHole;
+let timeUp = false;
+let score = 0;
+
+function randomTime(min, max) {
+  return Math.round(Math.random() * (max - min) + min);
+}
+
+function randomHole(holes) {
+  const idx = Math.floor(Math.random() * holes.length);
+  const hole = holes[idx];
+  if (hole === lastHole) {
+    return randomHole(holes);
+  }
+  lastHole = hole;
+  return hole;
+}
+function bonk(e) {
+  if (!e.isTrusted) return; //the .isTrusted property guarantees it is an actual mouse click and not someone using JS to hack the game
+  score++;
+  this.classList.remove("up");
+  scoreBoard.textContent = score;
+}
+
+function peep() {
+  const time = randomTime(200, 1000);
+  const hole = randomHole(holes);
+  hole.classList.add("up");
+  moles.forEach((mole) => mole.addEventListener("click", bonk));
+  setTimeout(() => {
+    hole.classList.remove("up");
+    if (!timeUp) peep();
+  }, time);
+}
+
+function startGame() {
+  score = 0;
+  scoreBoard.textContent = 0;
+  timeUp = false;
+  peep();
+  setTimeout(() => (timeUp = true), 10000);
+}
+
+startButton.addEventListener("click", startGame);
